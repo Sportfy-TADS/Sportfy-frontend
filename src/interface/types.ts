9f852994fa54dd66cb249f863767365e7e9a45d3
IamThiago-IT <thiagodossantos315@gmail.com>
2024-11-04 01:11:34 +0000

    add: eslint

--- src/interface/types.ts
+++ src/interface/types.ts
@@ -1,50 +1,50 @@
 export interface Achievement {
-  id: number;
-  userId: string | null;
-  title: string;
-  description: string;
-  category: string;
+  id: number
+  userId: string | null
+  title: string
+  description: string
+  category: string
 }
 
 export interface Sport {
-  id: number;
-  name: string;
-  description: string;
-  location: string;
-  schedule: string;
+  id: number
+  name: string
+  description: string
+  location: string
+  schedule: string
 }
 
 export interface DecodedToken {
-  sub: string;
-  role: string;
-  idUsuario: number;
-  exp: number;
-}
-
-export interface Post {
-  idPublicacao: number;
-  titulo: string;
-  descricao: string;
-  Usuario: {
-    idUsuario: number;
-    username: string;
-  };
-  listaUsuarioCurtida: number[];
-  listaComentario: Comment[];
+  sub: string
+  role: string
+  idUsuario: number
+  exp: number
 }
 
 export interface Comment {
-  idComentario: number;
-  descricao: string;
+  idComentario: number
+  descricao: string
   Usuario: {
-    idUsuario: number;
-    username: string;
-  };
-  listaUsuarioCurtida: number[];
+    idUsuario: number
+    username: string
+  }
+  listaUsuarioCurtida: number[]
+}
+
+export interface Post {
+  idPublicacao: number
+  titulo: string
+  descricao: string
+  Usuario: {
+    idUsuario: number
+    username: string
+  }
+  listaUsuarioCurtida: number[]
+  listaComentario: Comment[]
 }
 
 export interface User {
-  idUsuario: number;
-  username: string;
-  role: string;
+  idUsuario: number
+  username: string
+  role: string
 }
