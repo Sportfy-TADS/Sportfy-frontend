ab2f25be3548a5fcf6d530bea99bda35aae2a8e6
IamThiago-IT <thiagodossantos315@gmail.com>
2024-11-05 23:31:02 +0000

    docs: add docs

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,115 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Docmost',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'docmost', // Usually your GitHub org/user name.
+  projectName: 'docmost', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/docmost/docs/edit/main/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/docmost/docs/edit/main/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+        gtag: {
+          trackingID: 'G-15RC5C8G8H',
+          anonymizeIP: true,
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Docmost',
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'right',
+          label: 'Docs',
+        },
+        {
+          href: 'https://docmost.com',
+          label: 'Website',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/docmost/docmost',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    algolia: {
+      appId: '1YNLNIMGT9',
+      apiKey: '83977ee76ec0f9158a9f7805abd8ddb2',
+      indexName: 'most',
+      contextualSearch: true,
+      replaceSearchResultPathname: {
+        from: '/docs/',
+        to: '/',
+      },
+      searchParameters: {},
+      searchPagePath: 'search',
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['bash'],
+    },
+  } satisfies Preset.ThemeConfig,
+
+  scripts: [
+    {
+      src: 'https://data.docmost.com/js/plausible.js',
+      async: true,
+      defer: true,
+      'data-domain': 'docmost.com',
+    },
+  ],
+};
+
+export default config;
