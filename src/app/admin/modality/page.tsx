b7302ebc1640ad4dab273d68860b00a4c12b5b31
Thiago Santos <thiagodsantos316@gmail.com>
2024-11-17 17:48:43 +0000

    add: update

--- src/app/admin/modality/page.tsx
+++ src/app/admin/modality/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import {jwtDecode} from 'jwt-decode'
 import { toast } from 'sonner'
 
@@ -11,6 +11,7 @@ import Sidebar from '@/components/Sidebar'
 import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import {
   Select,
   SelectContent,
@@ -26,52 +27,111 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet'
 import { Skeleton } from '@/components/ui/skeleton'
+import { Modalidade } from '@/interface/types'
 
-async function fetchAdmins() {
+async function getModalidades() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/administrador/listar`,
+    `${process.env.NEXT_PUBLIC_API_URL}/modalidadeEsportiva/listar`,
   )
-  if (!res.ok) throw new Error('Erro ao buscar administradores.')
-  const data = await res.json()
-  return data.content // Adjust this based on the actual response structure
+  if (!res.ok) {
+    throw new Error('Erro ao buscar modalidades')
+  }
+  return await res.json()
 }
 
-
-async function createAdmin(newAdmin) {
+async function createModalidade(data: Partial<Modalidade>, token: string) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/administrador/cadastrar`,
+    `${process.env.NEXT_PUBLIC_API_URL}/modalidadeEsportiva`,
     {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify(newAdmin),
+      body: JSON.stringify(data),
     },
   )
-  if (!res.ok) throw new Error('Erro ao cadastrar administrador.')
-  return await res.json()
+  if (!res.ok) {
+    throw new Error('Erro ao cadastrar modalidade')
+  }
+  return res.json()
 }
 
-async function inactivateAdmin(id) {
+async function updateModalidade(data: Modalidade, token: string) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/administrador/inativar/${id}`,
+    `${process.env.NEXT_PUBLIC_API_URL}/modalidadeEsportiva`,
     {
-      method: 'PATCH',
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
     },
   )
-  if (!res.ok) throw new Error('Erro ao inativar administrador.')
+  if (!res.ok) {
+    throw new Error('Erro ao editar modalidade')
+  }
+  return res.json()
+}
+
+async function desativarModalidade(id: number, token: string) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/modalidadeEsportiva/desativar/${id}`,
+    {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  )
+  if (!res.ok) {
+    throw new Error('Erro ao desativar modalidade')
+  }
+  return res.json()
+}
+
+async function searchModalidade(nome: string) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/modalidadeEsportiva/buscar/${nome}`,
+  )
+  if (!res.ok) {
+    throw new Error('Erro ao buscar modalidade')
+  }
   return await res.json()
 }
 
-export default function AdminCrudPage() {
-  const [currentAdmin, setCurrentAdmin] = useState(null)
-  const [newAdmin, setNewAdmin] = useState({
-    name: '',
-    email: '',
-    username: '',
-    password: '',
+async function inscreverModalidade(
+  idAcademico: number,
+  idModalidadeEsportiva: number,
+  token: string,
+) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/modalidadeEsportiva/inscrever/${idAcademico}/${idModalidadeEsportiva}`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  )
+  if (!res.ok) {
+    throw new Error('Erro ao se inscrever na modalidade')
+  }
+  return res.json()
+}
+
+export default function AdminModalidadesPage() {
+  const [userId, setUserId] = useState<number | null>(null)
+  const [isAdmin, setIsAdmin] = useState(false)
+  const [modalidadeForm, setModalidadeForm] = useState<Partial<Modalidade>>({
+    nome: '',
+    descricao: '',
   })
-  const [showAdminsOnly, setShowAdminsOnly] = useState(true)
+  const [editMode, setEditMode] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [modalidades, setModalidades] = useState<Modalidade[]>([])
+  const [filter, setFilter] = useState('all')
   const router = useRouter()
   const queryClient = useQueryClient()
 
@@ -82,7 +142,7 @@ export default function AdminCrudPage() {
         router.push('/auth')
         return
       }
-      let decoded
+      let decoded: any
       try {
         decoded = jwtDecode(token)
         console.log('Decoded Token:', decoded) // Log do token decodificado
@@ -98,157 +158,247 @@ export default function AdminCrudPage() {
         router.push('/')
         return
       }
-      setCurrentAdmin({
-        id: decoded.idUsuario,
-        name: decoded.name,
-        email: decoded.email,
-        username: decoded.username,
-      })
+      setUserId(decoded.idUsuario)
+      setIsAdmin(true)
     }
     checkAdminStatus()
   }, [router])
 
-  const { data: admins = [], isLoading } = useQuery({
-    queryKey: ['admins'],
-    queryFn: fetchAdmins,
-    enabled: !!currentAdmin,
+  const token = localStorage.getItem('token') || ''
+
+  const { data: modalidadesData, isLoading } = useQuery({
+    queryKey: ['modalidades'],
+    queryFn: getModalidades,
+    onSuccess: (data) => setModalidades(data),
   })
 
-  const filteredAdmins = showAdminsOnly
-    ? admins.filter((admin) => admin.isAdmin)
-    : admins
+  const createMutation = useMutation({
+    mutationFn: (data: Partial<Modalidade>) => createModalidade(data, token),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['modalidades'])
+      toast.success('Modalidade cadastrada com sucesso!')
+    },
+    onError: () => {
+      toast.error('Erro ao cadastrar modalidade')
+    },
+  })
 
-  const handleCreateAdmin = async () => {
+  const updateMutation = useMutation({
+    mutationFn: (data: Modalidade) => updateModalidade(data, token),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['modalidades'])
+      toast.success('Modalidade atualizada com sucesso!')
+      setEditMode(false)
+    },
+    onError: () => {
+      toast.error('Erro ao atualizar modalidade')
+    },
+  })
+
+  const desativarMutation = useMutation({
+    mutationFn: (id: number) => desativarModalidade(id, token),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['modalidades'])
+      toast.success('Modalidade desativada com sucesso!')
+    },
+    onError: () => {
+      toast.error('Erro ao desativar modalidade')
+    },
+  })
+
+  const inscreverMutation = useMutation({
+    mutationFn: (idModalidadeEsportiva: number) =>
+      inscreverModalidade(userId!, idModalidadeEsportiva, token),
+    onSuccess: () => {
+      toast.success('Inscrição realizada com sucesso!')
+    },
+    onError: () => {
+      toast.error('Erro ao se inscrever na modalidade')
+    },
+  })
+
+  const handleCreateModalidade = () => {
+    if (!modalidadeForm.nome || !modalidadeForm.descricao) {
+      toast.error('Todos os campos são obrigatórios')
+      return
+    }
+    createMutation.mutate(modalidadeForm)
+  }
+
+  const handleEditModalidade = (modalidade: Modalidade) => {
+    setEditMode(true)
+    setModalidadeForm(modalidade)
+  }
+
+  const handleUpdateModalidade = () => {
+    if (!modalidadeForm.nome || !modalidadeForm.descricao) {
+      toast.error('Todos os campos são obrigatórios')
+      return
+    }
+    updateMutation.mutate(modalidadeForm as Modalidade)
+  }
+
+  const handleDesativarModalidade = (id: number) => {
+    desativarMutation.mutate(id)
+  }
+
+  const handleInscrever = (idModalidadeEsportiva: number) => {
+    inscreverMutation.mutate(idModalidadeEsportiva)
+  }
+
+  const handleSearch = async () => {
     try {
-      await createAdmin(newAdmin)
-      toast.success('Administrador cadastrado com sucesso.')
-      queryClient.invalidateQueries(['admins'])
-      setNewAdmin({ name: '', email: '', username: '', password: '' }) // Resetar o formulário
+      const data = await searchModalidade(searchTerm)
+      setModalidades([data])
     } catch (error) {
-      toast.error('Erro ao cadastrar o administrador.')
+      toast.error('Modalidade não encontrada')
     }
   }
 
-  const handleInactivateAdmin = async (id) => {
-    try {
-      await inactivateAdmin(id)
-      toast.success('Administrador inativado com sucesso.')
-      queryClient.invalidateQueries(['admins'])
-    } catch (error) {
-      toast.error('Erro ao inativar o administrador.')
-    }
+  if (isLoading) {
+    return <div>Loading...</div>
   }
+
+  const filteredModalidades =
+    filter === 'all'
+      ? modalidades
+      : modalidades.filter((mod) =>
+          filter === 'ativas' ? mod.status : !mod.status,
+        )
+
+
 
   return (
     <>
       <Header />
-      <div className="flex h-screen">
+      <div className="flex min-h-screen">
         <Sidebar className="flex-none" />
-        <div className="container mx-auto p-4 flex-1 overflow-y-auto">
-          <div className="flex flex-col md:flex-row justify-between items-center mb-6 space-y-4 md:space-y-0">
-            <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-100">
-              Gerenciar Administradores
-            </h1>
-            <Select
-              onValueChange={(value) => setShowAdminsOnly(value === 'admins')}
-            >
-              <SelectTrigger className="w-48">
-                <SelectValue placeholder="Filtrar usuários" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="admins">Mostrar apenas Admins</SelectItem>
-                <SelectItem value="all">Mostrar Todos</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-
-          {/* Botão para abrir o Sheet do cadastro */}
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="primary">Cadastrar Novo Administrador</Button>
-            </SheetTrigger>
-            <SheetContent position="right" size="lg">
-              <SheetHeader>
-                <SheetTitle>Cadastrar Administrador</SheetTitle>
-              </SheetHeader>
-              <div className="flex flex-col space-y-4 mt-6">
-                <Input
-                  placeholder="Nome"
-                  value={newAdmin.name}
-                  onChange={(e) =>
-                    setNewAdmin({ ...newAdmin, name: e.target.value })
-                  }
-                />
-                <Input
-                  placeholder="Email"
-                  value={newAdmin.email}
-                  onChange={(e) =>
-                    setNewAdmin({ ...newAdmin, email: e.target.value })
-                  }
-                />
-                <Input
-                  placeholder="Username"
-                  value={newAdmin.username}
-                  onChange={(e) =>
-                    setNewAdmin({ ...newAdmin, username: e.target.value })
-                  }
-                />
-                <Input
-                  placeholder="Senha"
-                  type="password"
-                  value={newAdmin.password}
-                  onChange={(e) =>
-                    setNewAdmin({ ...newAdmin, password: e.target.value })
-                  }
-                />
-                <Button
-                  onClick={handleCreateAdmin}
-                  variant="primary"
-                  className="mt-4"
-                >
-                  Salvar
-                </Button>
-              </div>
-            </SheetContent>
-          </Sheet>
-
-          {/* Listagem de administradores */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-            {isLoading
-              ? Array.from({ length: 6 }).map((_, index) => (
-                  <Skeleton key={index} className="w-full h-32 rounded-lg" />
-                ))
-              : filteredAdmins.map((admin) => (
-                  <Card
-                    key={admin.id}
-                    className="shadow-md bg-white dark:bg-gray-800"
-                  >
-                    <CardHeader>
-                      <CardTitle className="text-lg font-bold text-gray-800 dark:text-gray-100">
-                        {admin.name} ({admin.username})
-                      </CardTitle>
-                    </CardHeader>
-                    <CardContent className="flex flex-col space-y-2">
-                      <span className="text-gray-700 dark:text-gray-300">
-                        Email: {admin.email}
-                      </span>
-                      <div className="flex items-center justify-between">
-                        <Button
-                          onClick={() => setCurrentAdmin(admin)}
-                          variant="warning"
-                        >
-                          Editar
-                        </Button>
-                        <Button
-                          variant="destructive"
-                          onClick={() => handleInactivateAdmin(admin.id)}
-                        >
-                          Inativar
-                        </Button>
+        <div className="container mx-auto p-4 flex-1">
+          <div className="flex justify-between items-center mb-6">
+            <h1 className="text-3xl font-bold">Gerenciar Modalidades</h1>
+            <div className="flex space-x-4">
+              <Input
+                placeholder="Buscar por nome"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <Button
+                onClick={handleSearch}
+                className="bg-blue-500 hover:bg-blue-600"
+              >
+                Buscar
+              </Button>
+              <Select onValueChange={setFilter} defaultValue="all">
+                <SelectTrigger>
+                  <SelectValue placeholder="Filtrar" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="all">Todas</SelectItem>
+                  <SelectItem value="ativas">Ativas</SelectItem>
+                  <SelectItem value="desativadas">Desativadas</SelectItem>
+                </SelectContent>
+              </Select>
+              {isAdmin && (
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button className="bg-green-500 hover:bg-green-600">
+                      {editMode ? 'Editar Modalidade' : 'Cadastrar Modalidade'}
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent>
+                    <SheetHeader>
+                      <SheetTitle>
+                        {editMode
+                          ? 'Editar Modalidade'
+                          : 'Cadastrar Nova Modalidade'}
+                      </SheetTitle>
+                    </SheetHeader>
+                    <form className="space-y-4 mt-4">
+                      <div>
+                        <Label htmlFor="nome">Nome</Label>
+                        <Input
+                          id="nome"
+                          value={modalidadeForm.nome}
+                          onChange={(e) =>
+                            setModalidadeForm({
+                              ...modalidadeForm,
+                              nome: e.target.value,
+                            })
+                          }
+                        />
                       </div>
-                    </CardContent>
-                  </Card>
-                ))}
+                      <div>
+                        <Label htmlFor="descricao">Descrição</Label>
+                        <Input
+                          id="descricao"
+                          value={modalidadeForm.descricao}
+                          onChange={(e) =>
+                            setModalidadeForm({
+                              ...modalidadeForm,
+                              descricao: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                      <Button
+                        onClick={
+                          editMode
+                            ? handleUpdateModalidade
+                            : handleCreateModalidade
+                        }
+                        className="w-full bg-green-500 hover:bg-green-600"
+                      >
+                        {editMode
+                          ? 'Atualizar Modalidade'
+                          : 'Cadastrar Modalidade'}
+                      </Button>
+                    </form>
+                  </SheetContent>
+                </Sheet>
+              )}
+            </div>
+          </div>
+          <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {modalidades.map((modalidade: Modalidade) => (
+              <Card key={modalidade.idModalidadeEsportiva}>
+                <CardHeader>
+                  <CardTitle>{modalidade.nome}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>{modalidade.descricao}</p>
+                  <p>Status: {modalidade.status ? 'Ativa' : 'Desativada'}</p>
+                  {isAdmin ? (
+                    <div className="flex justify-end space-x-2 mt-4">
+                      <Button
+                        onClick={() => handleEditModalidade(modalidade)}
+                        className="bg-yellow-500 hover:bg-yellow-600"
+                      >
+                        Editar
+                      </Button>
+                      <Button
+                        onClick={() =>
+                          handleDesativarModalidade(
+                            modalidade.idModalidadeEsportiva,
+                          )
+                        }
+                        className="bg-red-500 hover:bg-red-600"
+                      >
+                        Desativar
+                      </Button>
+                    </div>
+                  ) : (
+                    <Button
+                      onClick={() =>
+                        handleInscrever(modalidade.idModalidadeEsportiva)
+                      }
+                      className="w-full bg-blue-500 hover:bg-blue-600 mt-4"
+                    >
+                      Inscrever-se
+                    </Button>
+                  )}
+                </CardContent>
+              </Card>
+            ))}
           </div>
         </div>
       </div>
